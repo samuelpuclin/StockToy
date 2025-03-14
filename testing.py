0e095979c4129763779fb12953a5adb3de313239
samuelpuclin <samuel.puclin@gmail.com>
2025-03-14 16:45:16 +1100

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-test = "2025-03-02"
-test2 = "2025/03/02"
-test3 = "2025.03.02"
-datetime.min
-print(datetime.strptime(test, "%Y-%m-%d"))
-print((datetime.strptime(test, "%Y-%m-%d") - datetime.min).days)
